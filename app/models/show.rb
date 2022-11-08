fa37e89da8e9260ee1a0e222c54af851eb362d88
Patrick Young <patrickyoung281@gmail.com>
2022-11-07 19:35:06 -0500

    commit tree 6d529b5e4bc96757c9dc7fa0823de4141b36f611

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+has_many :characters
+belongs_to :network
+has_many :actors, through: :characters
+
+def actors_list
+    self.actors.collect do |char|
+        "#{char.first_name} #{char.last_name}"
+    end
+end
+
 end
